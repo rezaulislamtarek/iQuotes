ea072c87d1ae486e5b29c202f7535dc407403bbd
rezaulislam <mrezaulislam.t@gmail.com>
2025-02-01 12:53:15 +0600

    Initial Commit

---
+++ iQuotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iQuotes
+//
+//  Created by Rezaul Islam on 1/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iQuotes/iQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuotesApp.swift
+//  iQuotes
+//
+//  Created by Rezaul Islam on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct iQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
