e6f5929fbb2630687c3cbcdeda8901a07ae40756
rezaulislam <mrezaulislam.t@gmail.com>
2025-04-28 23:25:36 +0600

    clip style

--- iQuotes/ContentView.swift
+++ iQuotes/ContentView.swift
@@ -9,59 +9,92 @@ import SwiftUI
 
 struct ContentView: View {
     let quotesProvider : QuoteProvider = QuoteProvider()
-    let size = UIScreen.main.bounds.size
+    @State private var currentIndex = 0
+    
+    
+    
     var body: some View {
-        VStack {
-            VStack{
-                Text("iQuotes")
-                    .font(.title)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal)
-                    .foregroundColor(.primary)
-                Text("Inspire, Create, Share.")
-                    .font(.callout)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal)
-                    .foregroundColor(.primary)
-            }.padding()
-            Spacer()
-            ScrollView( .horizontal, showsIndicators: false, content: {
-                LazyHStack(content: {
-                    
-                    ForEach(quotesProvider.getQuotes(), id: \.self.quote){ quote in
-                        VStack(spacing: 32 ){
-                            Text(quote.quote ?? "")
-                                .font(.title2)
-                                .frame(maxWidth: .infinity, alignment : .center)
-                            Text( "- \(quote.author ?? "")" )
-                                .frame(maxWidth: .infinity, alignment: .center)
-                        }
-                        .multilineTextAlignment(.center)
-                        .frame( width: size.width - 84 , height: 400, alignment: .leading)
-                        .padding()
-                        .background(.ultraThickMaterial.opacity(0.5))
-                        .cornerRadius(16)
-                        .padding(.horizontal, 4)
-                         
+        
+        GeometryReader{ proxy in
+            let size = proxy.size
+            TabView(selection: $currentIndex) {
+                ForEach(Array(quotesProvider.getQuotes().enumerated()), id: \.element.author) { index, quote in
+                    ZStack{
+                        Color.green
+                        QuoteCardView(quote: quote)
                         
                     }
-                })
-            })
-            .padding()
-            .scrollTargetLayout()
-            .scrollTargetBehavior(.viewAligned)
-            .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-            
-            Spacer()
+                    .frame(width: size.width)
+                    .tag(index)
+                    .rotationEffect(.degrees(-90))
+                    
+                    
+                }
+            }
+            .rotationEffect(.degrees(90))
+            .frame(width: proxy.size.height, alignment: .trailing)
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .frame(maxWidth: proxy.size.width)
         }
+        .ignoresSafeArea(.all, edges: .all)
+        
+        
         .background(
             LinearGradient(colors: [.green.opacity(0.4),  .blue.opacity(0.3)], startPoint: .topLeading, endPoint: .bottomTrailing)
         )
         .fontDesign(.serif)
         
     }
+    
+    
+    
 }
+
+
 
 #Preview {
     ContentView()
+}
+
+struct QuoteCardView: View {
+    let quote : Quotes
+    let size = UIScreen.main.bounds.size
+    var body: some View {
+        
+        VStack(spacing: 32 ){
+            Text(quote.quote ?? "")
+                .font(.title2)
+                .frame(maxWidth: .infinity, alignment : .center)
+            Text( "- \(quote.author ?? "")" )
+                .frame(maxWidth: .infinity, alignment: .center)
+        }
+        .multilineTextAlignment(.center)
+        .padding()
+        .frame(width: size.width, height: size.height)
+        .background(.ultraThickMaterial.opacity(0.5))
+        .background(.pink)
+        .ignoresSafeArea(.all)
+        
+        
+        
+        
+    }
+}
+
+struct HomeToolBarView: View {
+    var body: some View {
+        VStack{
+            Text("iQuotes")
+                .font(.title)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+                .foregroundColor(.primary)
+            Text("Inspire, Create, Share.")
+                .font(.callout)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+                .foregroundColor(.primary)
+        }.padding()
+        
+    }
 }
