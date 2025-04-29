091316df5f254fd54dbd50115db55b808c4907db
rezaulislam <mrezaulislam.t@gmail.com>
2025-04-29 23:17:01 +0600

    design ui

--- iQuotes/ContentView.swift
+++ iQuotes/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     let quotesProvider: QuoteProvider = QuoteProvider()
     @State private var currentIndex = 0
     private let gradient =  [Color.black.opacity(0.7), .clear,.clear,.clear,.clear, Color.black.opacity(0.2)]
+    @State private var tapCount = 0 // Count the number of taps
+    @State private var tapTimer: Timer? = nil // Timer to track the delay for tap recognition
+    @State private var showLikeAnimation = false
     
     var body: some View{
         GeometryReader{ proxy in
@@ -20,55 +23,66 @@ struct ContentView: View {
                 
                 ForEach(Array(quotesProvider.getQuotes().enumerated()), id: \.element.quote) { index, post in
                     
-                    
                     ZStack{
-                        
-                        ZStack{
-                            // if !showComment{
-                            Text("")
-                            .compositingGroup()
-                            // .ignoresSafeArea() // Ensure video covers the screen
-                            .aspectRatio(contentMode: .fit) // Ensure the video fills the space
-                            
-                            //}
-                            
-                           
-                            
-                        }
-                        .overlay {
-                            LinearGradient(colors: gradient, startPoint: .top, endPoint: .bottom)
-                        }
-                        
-                        
-                        ZStack{
-                            QuoteCardView(quote: post, size : proxy.size)
-                        }
-                        
-                        .frame(width: size.width)
+                        QuoteCardView(quote: post, size : proxy.size)
+                            .frame(width: size.width)
                     }
                     .rotationEffect(Angle(degrees: -90))
                     .ignoresSafeArea(.all, edges: .all)
                     .tag(index)
                     .onTapGesture(count: 1) {
-                         
+                        handleTap()
                     }
-                     
-                    
-                    
                 }
             }
             .rotationEffect(Angle(degrees: 90))
             .frame(width: proxy.size.height)
             .tabViewStyle(.page(indexDisplayMode: .never))
             .frame(maxWidth: proxy.size.width)
+            .overlay {
+                LottieView(filename: "love_lottie")
+                    .opacity(showLikeAnimation == true ? 1 : 0)
+            }
+        }
+        .ignoresSafeArea(.all, edges: .all)
+        .overlay (alignment: .top){
+            HomeToolBarView()
         }
         
-        .background(.green)
-        .ignoresSafeArea(.all, edges: .all)
+    }
     
+    func handleTap() {
+        tapCount += 1
+        
+        // Invalidate existing timer to reset the delay for detecting taps
+        tapTimer?.invalidate()
+        
+        // Set a new timer for tap detection
+        tapTimer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: false) { _ in
+            // Handle single tap action
+            if self.tapCount == 1 {
+                
+            }
+            
+            // Reset tap count after the action
+            self.tapCount = 0
+        }
+        
+        // Handle double tap or more taps
+        if tapCount == 2 {
+            showLikeAnimation = true // Show like animation for double taps
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showLikeAnimation = false // Hide like animation after 1 second
+            }
+        } else if tapCount > 2 {
+            showLikeAnimation = true // Show animation for additional taps
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showLikeAnimation = false // Hide like animation after 1 second
+            }
+        }
     }
 }
- 
+
 
 #Preview {
     ContentView()
@@ -82,24 +96,25 @@ struct QuoteCardView: View {
         VStack(spacing: 32 ){
             Text(quote.quote ?? "")
                 .font(.title2)
-                .foregroundStyle(.black)
                 .frame(maxWidth: .infinity, alignment : .center)
                 .padding()
             
             Text( "- \(quote.author ?? "")" )
                 .frame(maxWidth: .infinity, alignment: .center)
         }
+        .fontDesign(.serif)
+        
+        .foregroundStyle(.iqTextc)
         .multilineTextAlignment(.center)
+        .padding()
+        .padding(.vertical, 80)
+        .background(.ultraThickMaterial)
+        //.background(.ultraThickMaterial.opacity(0.5))
+        .cornerRadius(20)
+        .padding()
         .frame(width: size.width, height: size.height)
-        .background(.ultraThickMaterial.opacity(0.5))
-       
+        .background(.iqBgc)
          
-        .background(
-                    LinearGradient(colors: [.green.opacity(0.4),  .blue.opacity(0.3)], startPoint: .topLeading, endPoint: .bottomTrailing)
-                )
-        
-        
-        
         
     }
 }
@@ -118,6 +133,8 @@ struct HomeToolBarView: View {
                 .padding(.horizontal)
                 .foregroundColor(.primary)
         }.padding()
+            
+             
         
     }
 }
